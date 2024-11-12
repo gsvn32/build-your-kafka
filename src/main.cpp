3899082930d850cf20685502419b451a9418accc
Nikhil <gangisetty.nikhil@gmail.com>
2024-11-12 13:52:56 -0600

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-
+#define LOG(x) std::cout << x << std::endl
 int main(int argc, char* argv[]) {
     // Disable output buffering
     std::cout << std::unitbuf;
@@ -56,37 +56,67 @@ int main(int argc, char* argv[]) {
     
     // Uncomment this block to pass the first stage
     // 
-    //msg struct
-    // struct msghdr {
-    //     int32_t message_size;
-    //     int32_t correlation_id;
-
-    // };
+    bool print_to_console =0;
     
+    struct reqmodel {
+        int32_t message_size;
+        int16_t request_api_key;
+        int16_t request_api_version;
+        int32_t correlation_id;
+    };
+    struct resmodel {
+        int32_t message_size;
+        int32_t correlation_id;
+        int16_t error_code;
+    };
+    struct reqmodel request;
+    struct resmodel response;
     
     int client_fd = accept(server_fd, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addr_len);
     std::cout << "Client connected\n";
-    std::byte received [1024];
-    ssize_t rcflag = recv(client_fd,received,sizeof(received),0);
-    char sentmsg[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // before recv
-    if(rcflag==-1){
-        std::cerr<<"Failed recv";
-    }
-    else{
-        int j=4;
-        for(int i=8;i<12;i++)
-        {
-            sentmsg[j]=(int)received[i];
-            j++;
-        }
-            
-    }
+    std::byte rec_buffer[1024];
+    //recv data
+    recv(client_fd,&rec_buffer, 1024, 0);
+    std::memcpy(&(request.message_size),rec_buffer,4);
+    std::memcpy(&(request.request_api_key),rec_buffer+4,2);
+    std::memcpy(&(request.request_api_version),rec_buffer+6,2);
+    std::memcpy(&(request.correlation_id),rec_buffer+8,4);
     
-    ssize_t sent = send(client_fd,&sentmsg,sizeof(sentmsg),0);
-    if(sent==-1){
-        std::cerr<<"failed sendmsg";
+    //convert bytes from network byte order(always big endian) to host order(intel, small endian)
+    request.message_size=ntohl(request.message_size);
+    request.request_api_key=ntohs(request.request_api_key);
+    request.request_api_version=ntohs(request.request_api_version);
+    request.correlation_id=ntohl(request.correlation_id);
+
+    //Print to console
+    if(print_to_console ==1){
+        LOG(request.message_size);
+        LOG(request.request_api_key);
+        LOG(request.request_api_version);
+        LOG(request.correlation_id);
     }
 
+
+ 
+    //Logic for response calculation
+    response.correlation_id=request.correlation_id;
+    if(request.request_api_version != 4){
+        response.error_code=35;
+    }
+    
+    response.message_size=sizeof(response.correlation_id)+sizeof(response.error_code);
+
+
+    //convert bytes from host order(intel, small endian) to network byte order(always big endian)
+    response.correlation_id=htonl(response.correlation_id);
+    response.error_code=htons(response.error_code);
+    response.message_size=htonl(response.message_size);
+
+    //send response
+    send(client_fd,&(response.message_size),sizeof(response.message_size),0);
+    send(client_fd,&(response.correlation_id),sizeof(response.correlation_id),0);
+    send(client_fd,&(response.error_code),sizeof(response.error_code),0);
+      
     close(client_fd);
 
     close(server_fd);
